def257eaa08a0cd4ac65dc83b4e2f2ace2df8081
BohdanRomaniuk <bohdan2307@gmail.com>
2018-03-17 22:41:18 +0200

    Create empty solution with database and website projects

---
+++ src/database/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HomeSchooling
+{
+    class DatabaseContext
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/website/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
