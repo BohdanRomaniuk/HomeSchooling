725ae9f1e097199d5e695babaa9556f99f862ab1
BohdanRomaniuk <bohdan2307@gmail.com>
2018-03-30 23:09:53 +0300

    First database creation

---
+++ src/database/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace database.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string UserRole { get; set; }
+    }
+}
