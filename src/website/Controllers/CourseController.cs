fe8d3f44672614a8c896d481631071241b090e7e
BohdanRomaniuk <bohdan2307@gmail.com>
2018-04-03 12:32:17 +0300

    Start working on ReqestCourse

--- src/website/Controllers/CourseController.cs
+++ src/website/Controllers/CourseController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using website.Models;
+using Microsoft.AspNetCore.Http;
 
 namespace website.Controllers
 {
@@ -25,6 +26,16 @@ namespace website.Controllers
         public IActionResult ViewLesson(int id)
         {
             return View(db.Lessons.Where(l=>l.Id==id).SingleOrDefault());
+        }
+
+        public IActionResult RequestCourse(int id)
+        {
+            if(HttpContext.Session.GetInt32("role") !=null)
+            {
+                string role = HttpContext.Session.GetString("role");
+                return View("RequestCourse",role);
+            }
+            return View(HttpContext.Session.GetInt32("role"));
         }
     }
 }
