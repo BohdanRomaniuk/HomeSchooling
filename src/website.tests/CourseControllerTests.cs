a5d984d411008543b395aee5991c7ad91a2b6c6b
BohdanRomaniuk <bohdan2307@gmail.com>
2018-04-15 16:04:04 +0300

    Change stucture of project due to tests & finish testing homepage

---
+++ src/website.tests/CourseControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+
+namespace website.tests
+{
+    public class CourseControllerTests
+    {
+
+    }
+}
