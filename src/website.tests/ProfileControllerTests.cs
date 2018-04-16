ca8cbd3a04e7a78c1844ab9253f854549ed583d5
Roman Parobiy <romik1009@gmail.com>
2018-04-16 20:54:56 +0300

    Log in and out tests

--- src/website.tests/ProfileControllerTests.cs
+++ src/website.tests/ProfileControllerTests.cs
@@ -37,7 +37,7 @@ namespace website.tests
 
             Assert.True(actual);
         }
-       [Fact]
+        [Fact]
         public void LoginWrongUsernameTest()
         {
             Mock<IHomeSchoolingRepository> mock = new Mock<IHomeSchoolingRepository>();
@@ -56,6 +56,46 @@ namespace website.tests
             bool actual = (bool)(controller.Login(u2) as ViewResult).ViewData["IncorrectLogin"];
 
             Assert.True(actual);
+        }
+        [Fact]
+        public void LogingUserTest()
+        {
+            Mock<IHomeSchoolingRepository> mock = new Mock<IHomeSchoolingRepository>();
+            User u1 = new User { Id = 1, Name = "a1", UserName = "a1", Password = "a1", UserRole = "student" };
+            User u2 = new User { Id = 2, Name = "a2", UserName = "a2", Password = "a2", UserRole = "student" };
+            User u3 = new User { Id = 3, Name = "a3", UserName = "a3", Password = "a3", UserRole = "student" };
+            User[] users = new User[] { u1, u2, u3 };
+            mock.Setup(m => m.Users).Returns(users.AsQueryable());
+            Mock<HttpContext> mockHttpContext = new Mock<HttpContext>();
+            MockHttpSession mockSession = new MockHttpSession();
+            mockHttpContext.Setup(s => s.Session).Returns(mockSession);
+
+            ProfileController controller = new ProfileController(mock.Object);
+            controller.ControllerContext.HttpContext = mockHttpContext.Object;
+
+            bool redirected = (controller.Login(u2) is RedirectToRouteResult);
+
+            Assert.True(redirected);
+        }
+        [Fact]
+        public void LogingOutTest()
+        {
+            Mock<IHomeSchoolingRepository> mock = new Mock<IHomeSchoolingRepository>();
+            
+            Mock<HttpContext> mockHttpContext = new Mock<HttpContext>();
+            MockHttpSession mockSession = new MockHttpSession();
+            mockHttpContext.Setup(s => s.Session).Returns(mockSession);
+            mockSession["id"] = "1";
+            mockSession["username"] = "a";
+            mockSession["role"] = "teacher";
+            mockSession["name"] = "misha";
+
+            ProfileController controller = new ProfileController(mock.Object);
+            controller.ControllerContext.HttpContext = mockHttpContext.Object;
+
+            bool redirected = (controller.Logout() is RedirectToRouteResult);
+
+            Assert.True(redirected);
         }
     }
 }
