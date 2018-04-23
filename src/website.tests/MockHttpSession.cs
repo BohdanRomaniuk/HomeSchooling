7fd81075077fdb5a12f61cf1bb7a7877af70d7c1
BohdanRomaniuk <bohdan2307@gmail.com>
2018-04-23 19:49:57 +0300

    Start working on course controller tests with identity

--- src/website.tests/MockHttpSession.cs
+++ src/website.tests/MockHttpSession.cs
@@ -1,79 +1,79 @@
-﻿//using Microsoft.AspNetCore.Http;
-//using System;
-//using System.Collections.Generic;
-//using System.Text;
-//using System.Threading;
-//using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
-//namespace website.tests
-//{
-//    public class MockHttpSession : ISession
-//    {
-//        Dictionary<string, object> sessionStorage = new Dictionary<string, object>();
-//        public object this[string name]
-//        {
-//            get { return sessionStorage[name]; }
-//            set { sessionStorage[name] = value; }
-//        }
+namespace website.tests
+{
+    public class MockHttpSession : ISession
+    {
+        Dictionary<string, object> sessionStorage = new Dictionary<string, object>();
+        public object this[string name]
+        {
+            get { return sessionStorage[name]; }
+            set { sessionStorage[name] = value; }
+        }
 
-//        string ISession.Id
-//        {
-//            get
-//            {
-//                throw new NotImplementedException();
-//            }
-//        }
+        string ISession.Id
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
 
-//        bool ISession.IsAvailable
-//        {
-//            get
-//            {
-//                throw new NotImplementedException();
-//            }
-//        }
+        bool ISession.IsAvailable
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
 
-//        IEnumerable<string> ISession.Keys
-//        {
-//            get { return sessionStorage.Keys; }
-//        }
+        IEnumerable<string> ISession.Keys
+        {
+            get { return sessionStorage.Keys; }
+        }
 
-//        public Task CommitAsync(CancellationToken cancellationToken = default(CancellationToken))
-//        {
-//            throw new NotImplementedException();
-//        }
+        public Task CommitAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            throw new NotImplementedException();
+        }
 
-//        public Task LoadAsync(CancellationToken cancellationToken = default(CancellationToken))
-//        {
-//            throw new NotImplementedException();
-//        }
+        public Task LoadAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            throw new NotImplementedException();
+        }
 
-//        void ISession.Clear()
-//        {
-//            sessionStorage.Clear();
-//        }
+        void ISession.Clear()
+        {
+            sessionStorage.Clear();
+        }
 
-//        void ISession.Remove(string key)
-//        {
-//            sessionStorage.Remove(key);
-//        }
+        void ISession.Remove(string key)
+        {
+            sessionStorage.Remove(key);
+        }
 
-//        void ISession.Set(string key, byte[] value)
-//        {
-//            sessionStorage[key] = value;
-//        }
+        void ISession.Set(string key, byte[] value)
+        {
+            sessionStorage[key] = value;
+        }
 
-//        bool ISession.TryGetValue(string key, out byte[] value)
-//        {
-//            if (sessionStorage[key] != null)
-//            {
-//                value = Encoding.ASCII.GetBytes(sessionStorage[key].ToString());
-//                return true;
-//            }
-//            else
-//            {
-//                value = null;
-//                return false;
-//            }
-//        }
-//    }
-//}
+        bool ISession.TryGetValue(string key, out byte[] value)
+        {
+            if (sessionStorage[key] != null)
+            {
+                value = Encoding.ASCII.GetBytes(sessionStorage[key].ToString());
+                return true;
+            }
+            else
+            {
+                value = null;
+                return false;
+            }
+        }
+    }
+}
