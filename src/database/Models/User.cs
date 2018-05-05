de4cb348ded7d2bfe39d243016ef25bdada809f0
BohdanRomaniuk <bohdan2307@gmail.com>
2018-05-05 21:48:08 +0300

    Simplify user model

--- src/database/Models/User.cs
+++ src/database/Models/User.cs
@@ -6,15 +6,7 @@ namespace database.Models
 {
     public class User: IdentityUser
     {
-        //[Key]
-        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        //public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-        //public string UserName { get; set; }
-        //[Required]
-        //public string Password { get; set; }
-        //[Required]
-        //public string UserRole { get; set; }
     }
 }
