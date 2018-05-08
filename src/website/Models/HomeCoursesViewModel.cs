4f2287cc0b219c751cef66037ccabea2d4c996e0
Modest97 <modest.radomskyy19971411@gmail.com>
2018-05-08 23:57:22 +0300

    Add categories to courses

---
+++ src/website/Models/HomeCoursesViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace website.Models
+{
+    public class HomeCoursesViewModel
+    {
+        public string Category { get; set; }
+        public IEnumerable<HomeViewModel> Courses { get; set; }
+        public HomeCoursesViewModel(IEnumerable<HomeViewModel> _courses, string _category)
+        {
+            Category = _category;
+            Courses = _courses;
+        }
+    }
+}
