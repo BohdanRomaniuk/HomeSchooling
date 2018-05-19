20d301128a7957e27c7b206b01c3852dd7a86b80
romanparobiy <romik1009@gmail.com>
2018-05-19 14:08:43 +0300

    Fixed HomeController tests

--- src/website.tests/HomeControllerTests.cs
+++ src/website.tests/HomeControllerTests.cs
@@ -17,72 +17,103 @@ namespace website.tests
 {
     public class HomeControllerTests
     {
-        //[Fact]
-        //public void CoursesList()
-        //{
-        //    //Arrange
-        //    Mock<IHomeSchoolingRepository> mock = new Mock<IHomeSchoolingRepository>();
-        //    User teacher = new User { Name = "Музичук А.О.", UserName = "anatoliy.muzychuk"};
-        //    Course[] courses = new Course[]
-        //    {
-        //        new Course("Проектування програмних систем", "Опис курсу", teacher, Convert.ToDateTime("05.03.2018 13:10"), Convert.ToDateTime("12.03.2018 13:10")),
-        //        new Course("Програмування мовою с++", "Опис курсу", teacher, Convert.ToDateTime("05.03.2018 13:10"), Convert.ToDateTime("12.03.2018 13:10"))
-        //    };
-        //    mock.Setup(m => m.Courses).Returns(courses.AsQueryable());
+        [Fact]
+        public void AllCoursesListTest()
+        {
+            //Arrange
+            Mock<IHomeSchoolingRepository> mock = new Mock<IHomeSchoolingRepository>();
+            User teacher = new User { Name = "Музичук А.О.", UserName = "anatoliy.muzychuk" };
+            Course[] courses = new Course[]
+            {
+                new Course("Проектування програмних систем", "Опис курсу", teacher, Convert.ToDateTime("05.03.2018 13:10"), Convert.ToDateTime("12.03.2018 13:10")) { Category = "1"},
+                new Course("Програмування мовою с++", "Опис курсу", teacher, Convert.ToDateTime("05.03.2018 13:10"), Convert.ToDateTime("12.03.2018 13:10")) { Category = "1"}
+            };
+            mock.Setup(m => m.Courses).Returns(courses.AsQueryable());
 
-        //    HomeController controller = new HomeController(mock.Object, null, null);
-        //    controller.ControllerContext = new ControllerContext
-        //    {
-        //        HttpContext = new DefaultHttpContext
-        //        {
-        //            User = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
-        //            {
-        //                new Claim(ClaimTypes.Name, "anatoliy.muzychuk"),
-        //                new Claim(ClaimTypes.Role, "Teacher")
-        //            }, "Authentication"))
-        //        }
-        //    };
+            HomeController controller = new HomeController(mock.Object, null, null);
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim(ClaimTypes.Name, "anatoliy.muzychuk"),
+                        new Claim(ClaimTypes.Role, "Teacher")
+                    }, "Authentication"))
+                }
+            };
 
-        //    //Act
-        //    var result = (controller.Index() as ViewResult).Model as List<HomeViewModel>;
+            //Act
+            var result = ((controller.Index() as ViewResult).Model as HomeCoursesViewModel).Courses;
 
-        //    //Assert
-        //    Assert.Equal(courses.Length, result.Count());
-        //}
+            //Assert
+            Assert.Equal(courses.Length, result.Count());
+        }
+        [Fact]
+        public void CategoryCoursesListTest()
+        {
+            //Arrange
+            Mock<IHomeSchoolingRepository> mock = new Mock<IHomeSchoolingRepository>();
+            User teacher = new User { Name = "Музичук А.О.", UserName = "anatoliy.muzychuk" };
+            Course[] courses = new Course[]
+            {
+                new Course("Проектування програмних систем", "Опис курсу", teacher, Convert.ToDateTime("05.03.2018 13:10"), Convert.ToDateTime("12.03.2018 13:10")) { Category = "1"},
+                new Course("Програмування мовою с++", "Опис курсу", teacher, Convert.ToDateTime("05.03.2018 13:10"), Convert.ToDateTime("12.03.2018 13:10")) { Category = "2"}
+            };
+            mock.Setup(m => m.Courses).Returns(courses.AsQueryable());
 
-        //[Fact]
-        //public void CoursesSearch()
-        //{
-        //    //Arrange
-        //    Mock<IHomeSchoolingRepository> mock = new Mock<IHomeSchoolingRepository>();
-        //    User teacher = new User { Name = "Музичук А.О.", UserName = "anatoliy.muzychuk" };
-        //    Course[] courses = new Course[]
-        //    {
-        //        new Course("Проектування програмних систем", "Опис курсу", teacher, Convert.ToDateTime("05.03.2018 13:10"), Convert.ToDateTime("12.03.2018 13:10")),
-        //        new Course("Програмування мовою C++", "Опис курсу", teacher, Convert.ToDateTime("05.03.2018 13:10"), Convert.ToDateTime("12.03.2018 13:10")),
-        //        new Course("Програмування мовою C#", "Опис курсу", teacher, Convert.ToDateTime("05.03.2018 13:10"), Convert.ToDateTime("12.03.2018 13:10"))
-        //    };
-        //    mock.Setup(m => m.Courses).Returns(courses.AsQueryable());
-        //    HomeController controller = new HomeController(mock.Object, null, null);
-        //    string searchCourseName = "Програмування";
-        //    var expectedResult = courses.Where(c => c.Name.Contains(searchCourseName));
-        //    controller.ControllerContext = new ControllerContext
-        //    {
-        //        HttpContext = new DefaultHttpContext
-        //        {
-        //            User = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
-        //            {
-        //                new Claim(ClaimTypes.Name, "anatoliy.muzychuk"),
-        //                new Claim(ClaimTypes.Role, "Teacher")
-        //            }, "Authentication"))
-        //        }
-        //    };
+            HomeController controller = new HomeController(mock.Object, null, null);
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim(ClaimTypes.Name, "anatoliy.muzychuk"),
+                        new Claim(ClaimTypes.Role, "Teacher")
+                    }, "Authentication"))
+                }
+            };
 
-        //    //Act
-        //    var result = (controller.Index(searchCourseName) as ViewResult).Model as List<HomeViewModel>;
+            //Act
+            var result = ((controller.Index(category:"1") as ViewResult).Model as HomeCoursesViewModel).Courses;
+            var expected = 1;
 
-        //    //Assert
-        //    Assert.Equal(expectedResult.Count(), result.Count());
-        //}
+            //Assert
+            Assert.Equal(expected, result.Count());
+        }
+        [Fact]
+        public void SearchCoursesListTest()
+        {
+            //Arrange
+            Mock<IHomeSchoolingRepository> mock = new Mock<IHomeSchoolingRepository>();
+            User teacher = new User { Name = "Музичук А.О.", UserName = "anatoliy.muzychuk" };
+            Course[] courses = new Course[]
+            {
+                new Course("Проектування програмних систем", "Опис курсу", teacher, Convert.ToDateTime("05.03.2018 13:10"), Convert.ToDateTime("12.03.2018 13:10")) { Category = "1"},
+                new Course("Програмування мовою с++", "Опис курсу", teacher, Convert.ToDateTime("05.03.2018 13:10"), Convert.ToDateTime("12.03.2018 13:10")) { Category = "2"}
+            };
+            mock.Setup(m => m.Courses).Returns(courses.AsQueryable());
+
+            HomeController controller = new HomeController(mock.Object, null, null);
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim(ClaimTypes.Name, "anatoliy.muzychuk"),
+                        new Claim(ClaimTypes.Role, "Teacher")
+                    }, "Authentication"))
+                }
+            };
+
+            //Act
+            var result = ((controller.Index(name: "Проектування") as ViewResult).Model as HomeCoursesViewModel).Courses;
+            var expected = 1;
+
+            //Assert
+            Assert.Equal(expected, result.Count());
+        }
     }
 }
